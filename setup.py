9d4b45c8c0d5c9861d63f643617d5640ad4e5902
marmiha <miha.markez@gmail.com>
2023-12-20 07:11:07 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='xai_framework',
+    version='0.1',
+    description='Explainable AI Framework',
+    author='',
+    author_email='',
+    url='https://github.com/marmiha/xai_framework',
+    packages=[
+        'xai_framework'
+    ],
+)
